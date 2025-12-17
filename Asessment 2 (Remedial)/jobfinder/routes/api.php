114ce230f8d00e5ea9f930808e783c7ac83c2c60
Anantha <thamhrdk@gmail.com>
2025-12-17 09:45:10 +0800

    Pratikum 12

--- Asessment 2 (Remedial)/jobfinder/routes/api.php
+++ Asessment 2 (Remedial)/jobfinder/routes/api.php
@@ -2,15 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Semua API routes sekarang ditangani oleh Module JobFinder
-| Lihat: Modules/JobFinder/routes/api.php
-|
-| Endpoint tersedia di: /api/v1/module/...
-|
-*/
 
